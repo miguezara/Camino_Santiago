b2cac67709fa92fce680fdda46dcfa89be95b2be
Miguel Ángel Alcaide Cepas <zara_migue@hotmail.com>
2026-07-25 19:36:17 +0200

    Configurar despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins:[react()], server:{host:'0.0.0.0'}, build:{outDir:'dist'} });
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Camino_Santiago/',
+  server: {
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist'
+  }
+});
